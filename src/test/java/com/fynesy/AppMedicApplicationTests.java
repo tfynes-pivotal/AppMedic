eecc3d196232afbc5ba1f697f7812f06aee3a1b3
tfynes-pivotal <tfynes@Thomass-MacBook-Pro-3.local>
2019-12-05 12:04:49 -0500

    hello world rest app

---
+++ src/test/java/com/fynesy/AppMedicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fynesy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMedicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
